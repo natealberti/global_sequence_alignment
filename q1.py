82854cde51377a865a74a4e579936756f6114538
fire774 <65456795+natealberti@users.noreply.github.com>
2023-10-18 12:18:29 -0700

    fixed/finished q1

--- q1.py
+++ q1.py
@@ -7,58 +7,81 @@ s1 = "WKMDKSYWLFVREKKTDLCM"
 n = len(s1)
 s2 = "AIDDKSWAFVRECKTDQTW"
 m = len(s2)
-indel_penalty = 5
+indel_penalty = -5
 
-# initialize n x m matrix of zeros
-scores = np.zeros((n, m))
-backtrack = np.zeros((n, m))
+# initialize scoring and backtrack matrices
+scores = np.zeros((n+1, m+1))
+backtrack = np.zeros((n+1, m+1))
 
-for i in range(1, n):
-    for j in range(1, m):
+# fill in first row and column of scoring matrix with indel penalties
+for i in range(1, n+1):
+    scores[i][0] = indel_penalty*i
+for j in range(1, m+1):
+    scores[0][j] = indel_penalty*j
+
+# fill scoring matrix via dynamic programming
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        # find the score of each cell with match, insert, delete
         match = scores[i-1][j-1] + mat[s1[i-1]][s2[j-1]]
-        _delete = scores[i-1][j] - indel_penalty
-        _insert = scores[i][j-1] - indel_penalty
-        e = max(match, _delete, _insert)
-        if e == _delete:
-            backtrack[i][j] = -2
-        elif e == _insert:
-            backtrack[i][j] = -1
-        else:
+        _delete = scores[i-1][j] + indel_penalty
+        _insert = scores[i][j-1] + indel_penalty
+
+        # find the best-scoring choice
+        # fill in score and backtrack matrices appropriately
+        choice = max(match, _insert, _delete)
+        if choice == match:
             backtrack[i][j] = 1
-        scores[i][j] = max(
-            scores[i-1][j-1] + mat[s1[i-1]][s2[j-1]],   # match/mismatch
-            scores[i-1][j] - indel_penalty,             # delete
-            scores[i][j-1] - indel_penalty              # insert
-        )
-
-def recoverLCS(backtrack, s1, s2):
-    s1_aligned = ""
-    s2_aligned = ""
-    i = len(s1)-1
-    j = len(s2)-1
-    s1_accumulator = 0
-    s2_accumulator = 0
-    while i > 0 and j > 0:
-        if backtrack[i][j] == -2:
-            s1_aligned += s1[s1_accumulator]
-            s1_accumulator += 1
-            s2_aligned += "_"
-            i -= 1
-        elif backtrack[i][j] == -1:
-            s2_aligned += s2[s2_accumulator]
-            s2_accumulator += 1
-            s1_aligned += "_"
-            j -= 1
+            scores[i][j] = match
+        elif choice == _delete:
+            backtrack[i][j] = -2
+            scores[i][j] = _delete
         else:
-            s1_aligned += s1[s1_accumulator]
-            s1_accumulator += 1
-            s2_aligned += s2[s2_accumulator]
-            s2_accumulator += 1
-            i -= 1
-            j -= 1
-    return [s1_aligned, s2_aligned]
+            backtrack[i][j] = -1
+            scores[i][j] = _insert
+           
+           
+# recover the aligned strings via backtrack matrix  #
+s1_aligned = ""
+s2_aligned = ""
+i = len(s1)
+j = len(s2)
 
-print("score: " + str(scores[n-1][m-1]))
-s1_aligned, s2_aligned = recoverLCS(backtrack, s1, s2)
+# start from sink of scoring matrix
+# use backtrack arrows to return to source
+#   1 = diagonal move
+#  -2 = down move
+#  -1 = right move
+while (i > 0 or j > 0) and backtrack[i][j] != 0:
+    # diagonal move
+    if i > 0 and j > 0 and backtrack[i][j] == 1:
+        s1_aligned = s1[i-1] + s1_aligned
+        s2_aligned = s2[j-1] + s2_aligned
+        i -= 1
+        j -= 1
+    # down move
+    elif i > 0 and backtrack[i][j] == -2:
+        s1_aligned = s1[i-1] + s1_aligned
+        s2_aligned = "_" + s2_aligned
+        i -= 1
+    # right move
+    elif j > 0 and backtrack[i][j] == -1:
+        s1_aligned = "_" + s1_aligned
+        s2_aligned = s2[j-1] + s2_aligned
+        j -= 1
+
+# perform any deletions at start if necessart
+while i > 0:
+    s1_aligned = s1[i-1] + s1_aligned
+    s2_aligned = "_" + s2_aligned
+    i -= 1
+
+# perfomr any insertions at the start if necessary
+while j > 0:
+    s1_aligned = "_" + s1_aligned
+    s2_aligned = s2[j-1] + s2_aligned
+    j -= 1
+
+print("score: " + str(scores[n][m]))
 print(s1_aligned)
 print(s2_aligned)
